f350d3ccd84cf9cf3e598edb649dc8cc128c2caf
Kunal <74914319+kunrex@users.noreply.github.com>
2024-09-15 13:47:31 +0530

    Update sound-enum.ts

--- services/sound/enums/sound-enum.ts
+++ services/sound/enums/sound-enum.ts
@@ -2,9 +2,6 @@ export enum SoundEnum {
   error = 'error',
   click = 'click',
   
-  stopRecording = 'stopRecording',
-  startRecording = 'startRecording',
-
   save = 'save',
   discard = 'discard',
 }
